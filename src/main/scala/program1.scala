ed75d83cc9c14694ca8bcec881a3c70cd50fbc41
garima <garimasablok01@gmail.com>
2023-04-04 21:23:11 +0530

    commit my project

--- src/main/scala/program1.scala
+++ src/main/scala/program1.scala
@@ -1,31 +1,41 @@
-import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.types.DataType
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 
 import scala.Console.println
+import scala.reflect.io.Path
 
 object program1 {
 
-  def main(args:Array[String]):Unit={
+  def main(args:Array[String]):Unit= {
 
-    val spark=SparkSession.builder()
+    val spark = SparkSession.builder()
       .appName("count")
       .master("local[3]")
       .getOrCreate()
 
-    val countDf=spark.read
+    val countDf = spark.read
       .format("csv")
-      .option("Header","True")
-      .option("mode","FAILFAST")
+      .option("Header", "True")
+      .option("mode", "FAILFAST")
       .load("Program1")
-       countDf.show(false)
-       countDf.count()
-       println(countDf.count())
+    countDf.show(false)
+    countDf.count()
+    println(countDf.count())
 
-    countDf.write
+
+    countRead(spark,"Program1").show()
+
+    /* countDf.write
       .format("json")
       .mode(SaveMode.Overwrite)
       .option("path","Program1/output")
       .save()
+      //countDf.repartition(2).write.json("Program1/output")*/
 
+
+    def countRead(spark: SparkSession, path: String): DataFrame={
+    spark.read.csv(path)
+    }
 
   }
 }
